c0e26812df2d05d2a93382469ee0707f265ccd1e
Kim <kimescabarte@gmail.com>
2022-08-08 20:03:33 +0800

    Added labels for grid size and color mode Updated css

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
 const drawArea = document.getElementById("draw-area");
 const resetButton = document.getElementById("reset");
 const sizeButton = document.getElementById("size");
+const colorButton = document.getElementById("color");
+const colorLabel =  document.getElementById("colorLabel");
+const sizeLabel =  document.getElementById("sizeLabel");
 resetButton.addEventListener("click",resetGrid);
-sizeButton.addEventListener("click",setGridSize)
+sizeButton.addEventListener("click",setGridSize);
+colorButton.addEventListener("click",setColorMode);
 
-const grid = document.createElement("div");
+let isBlack = true; //toggle variable
 
-let numGrid = 16;
+let numGrid = 16; //default grid size
 
+//creates the drawing area using the size set by numGrid
 function drawGrid(){
-    drawArea.innerHTML="";
+    sizeLabel.innerText = `Grid Size: ${numGrid} x ${numGrid}`;
+    drawArea.innerHTML = "";
     for(let i = 0; i< numGrid*numGrid; i++){
         const grid = document.createElement("div");
         grid.classList.add("grid");
@@ -19,7 +25,7 @@ function drawGrid(){
     }  
 }
 
-
+//gets grid size from user input
 function setGridSize(){
     numGrid = prompt("Enter Grid Size(2 to 100)");
     
@@ -35,24 +41,45 @@ function setGridSize(){
     
 }
 
+//changes the div color 
 function changeColor(e){
     let newColor = randomizeColor();
     this.style.backgroundColor = newColor;
-    //this.parentElement.parentElement.parentElement.style.backgroundColor = newColor;
+}
+
+//toggles between black and random color
+function setColorMode(){
+    if(isBlack){
+        colorButton.innerHTML = "Black Mode";
+        colorLabel.innerText = "Color: Random";
+    }else{
+        colorButton.innerHTML = "Color Mode";
+        colorLabel.innerText = "Color: Black";
+    }
+    isBlack = !isBlack;
 
 }
+
+//randomizes color every div
 function randomizeColor() {
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    let bgColor;
+    if(isBlack){
+        bgColor = "black";
+    }else{
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+        bgColor = `rgb(${x}, ${y}, ${z})`
+    }
     return bgColor;
 }
 
+//erases all colors from the grid
 function resetGrid(){
     drawGrid();
 }
 
+//creates the default grid on load
 window.onload = function() {
     drawGrid();
   };
